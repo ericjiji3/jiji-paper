004c27b080f322e07d00477018c9ce21c7badbb4
eric.jiji3 <eric.ji3333@gmail.com>
2026-02-11 23:15:01 -0500

    dkfdk

--- screens.py
+++ screens.py
@@ -10,7 +10,7 @@ def lock_screen(width, height):
     font_big = ImageFont.truetype("assets/Vanosky-Bold.otf", 48)
     font_small = ImageFont.truetype("assets/Acidic.TTF", 20)
 
-    draw.text((20, 40), "FUCK BOSS BABY MARCOS SHUT THE FUCK UP", font=font_big, fill=0)
+    draw.text((20, 40), "FUCK BOSS BABY", font=font_big, fill=0)
     draw.text((20, 110), time.strftime("%H:%M:%S"), font=font_big, fill=0)
     draw.text((20, 200), "Press button to unlock", font=font_small, fill=0)
 
@@ -21,7 +21,7 @@ def status_screen(width, height):
     image = Image.new("1", (width, height), 255)
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype("assets/Vanosky-Bold.otf", 28)
+    font = ImageFont.truetype("assets/Acidic.TTF", 28)
 
     draw.text((20, 40), "System Status", font=font, fill=0)
     draw.text((20, 90), "WiFi: Connected", font=font, fill=0)
