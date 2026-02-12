e09d6554e8ab89299e63d1ce4803ad7f5b3177b0
eric.jiji3 <eric.ji3333@gmail.com>
2026-02-11 22:34:38 -0500

    added fil shortcuts

--- display.py
+++ display.py
@@ -1,4 +1,10 @@
 # display.py
+import sys
+import os
+
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
 
 from waveshare_epd import epd4in2_V2
 from PIL import Image
