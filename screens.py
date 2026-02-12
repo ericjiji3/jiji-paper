a5bbc5e622c5303b12933eb3f0b4bde1c430680e
eric.jiji3 <eric.ji3333@gmail.com>
2026-02-11 23:56:27 -0500

    dkfn

--- screens.py
+++ screens.py
@@ -10,7 +10,7 @@ def lock_screen(width, height):
     font_big = ImageFont.truetype("assets/Vanosky-Bold.otf", 48)
     font_small = ImageFont.truetype("assets/Acidic.TTF", 20)
 
-    draw.text((20, 40), "To Sonam ~", font=font_big, fill=0)
+    draw.text((20, 40), "To Sonam", font=font_big, fill=0)
     # draw.text((20, 110), time.strftime("%H:%M:%S"), font=font_big, fill=0)
     draw.text((20, 100), "Roses are red", font=font_small, fill=0)
     draw.text((20, 130), "Violets are blue", font=font_small, fill=0)
